036a165c60ddc3178a583b82be7d460b1dd496e2
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-02 12:22:11 +0200

    addition functions 8

--- script.js
+++ script.js
@@ -5,3 +5,64 @@ function sum(a,b) {
    console.log(sum);
 }
 sum(4,5);
+
+console.log('------------- #4b')
+
+function subtract(a,b) {
+    let substract = a - b;
+    console.log(substract);
+}
+subtract(10,7);
+
+console.log('------------- #4c')
+
+function multiply(a,b) {
+    let multiply = a * b;
+    console.log(multiply);
+}
+
+multiply(6,7);
+
+
+console.log('------------- #4d')
+
+function divide(a,b) {
+    let division = a / b;
+    console.log(division);
+}
+
+divide(6,9);
+
+console.log('------------- #5')
+
+function output(n) {
+for (i = 1; i<=n; i++){
+    console.log(i);
+}
+}
+output(5);
+
+console.log('------------- #6')
+
+function output(n) {
+for (i = n; i>0; i--){
+    console.log(i);
+}
+}
+output(5);
+
+console.log('------------- #7')
+ 
+function pow(x,n) {
+    let result = x**n;
+    console.log(result);
+}
+pow(2,10);
+
+
+console.log('------------- #8')
+
+function isBigger(a,b) {
+    return a > b;
+}
+isBigger(5, -1);
